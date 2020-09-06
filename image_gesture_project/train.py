0a17bba9523bfb8b96b1737923a8685f58e90c8b
mikechen66 <875661606@qq.com>
2020-09-07 00:26:42 +0800

    Improve the train script
    Improve the model by eliminating the prediction functionality.

--- image_gesture_project/train.py
+++ image_gesture_project/train.py
@@ -12,10 +12,11 @@
 
 import tensorflow as tf
 from keras.preprocessing.image import ImageDataGenerator
-from keras import optimizers
+# -from keras import optimizers
 import matplotlib.pyplot as plt
 from alexnet import AlexNet
 from keras.models import load_model
+
 from keras import models
 from keras.preprocessing import image
 import numpy as np
@@ -31,58 +32,55 @@ for gpu in gpus:
 
 # Assign the global arguments 
 EPOCHS = 32
-BATCH_SIZE = 64
-image_width = 150
-image_height = 150
+BATCH_SIZE = 100
+image_width = 227
+image_height = 227
 channels = 3
-num_classes = 1
+num_classes = 10
 
 
 # Call the cnn/alexnet model 
 model = AlexNet((image_width,image_height,channels), num_classes)
 
 # Model configuration
-model.compile(optimizer=optimizers.RMSprop(lr=1e-4),
-              loss='binary_crossentropy',
+model.compile(optimizer=tf.keras.optimizers.Adam(0.001),
+              loss='categorical_crossentropy',
               metrics=['acc'])
 
 # Summary
-model.summary
+model.summary()
 
 
 # Directories for the datasets 
 train_dir = '/home/mike/Documents/image_gesture/dset_data/train'
 val_dir   = '/home/mike/Documents/image_gesture/dset_data/validation'
-test_dir   = '/home/mike/Documents/image_gesture/dset_data/train'
+test_dir  = '/home/mike/Documents/image_gesture/dset_data/test'
 
 
 # Preprocess the images
 train_datagen = ImageDataGenerator(rescale=1.0/255)
+
 train_generator = train_datagen.flow_from_directory(train_dir,
                                                     target_size=(image_width,image_height),
                                                     batch_size=BATCH_SIZE,
-                                                    class_mode='binary')
+                                                    class_mode='categorical')
+
 train_num = train_generator.samples
 
-validation_generator = train_datagen.flow_from_directory(val_dir,
-                                                         target_size=(image_width,image_height),
-                                                         batch_size=BATCH_SIZE,
-                                                         class_mode='binary')
-val_num = validation_generator.samples
-
-
 test_datagen = ImageDataGenerator(rescale=1.0/255)
-test_generator = train_datagen.flow_from_directory(test_dir,
-                                                   target_size=(image_width,image_height),
-                                                   batch_size=BATCH_SIZE,
-                                                   class_mode='binary')
-test_num = test_generator.samples
+
+val_generator = test_datagen.flow_from_directory(val_dir,
+                                                 target_size=(image_width,image_height),
+                                                 batch_size=BATCH_SIZE,
+                                                 class_mode='categorical')
+
+val_num = val_generator.samples
 
 
 # Get the batch shape
 for data_batch, label_batch in train_generator:
     print("data batch shape:", data_batch.shape)
-    print("labels batch shape:", label_batch)
+    print("label batch shape:", label_batch)
     
     break
 
@@ -102,12 +100,10 @@ history = model.fit(train_generator,
 
 
 # Save the model 
-# -model.save('/home/mike/Documents/image_gesture/leapGestRecog_small_1.h5')
-# -model.save('/home/mike/Documents/image_gesture/leapGestRecog_small_2.h5')
+# -model.save('/home/mike/Documents/image_gesture/leapGestRecog_small_categorical.h5')
 
 
 # Evaluate the model with visulizing the result 
-
 acc = history.history['acc']
 val_acc = history.history['val_acc']
 loss = history.history['loss']
@@ -127,44 +123,6 @@ plt.plot(epochs, val_loss, 'b', label='Validation loss')
 plt.title('Training and validation loss')
 plt.legend()
 
-plt.show()
-
-
-# Select the saved model 
-model = load_model('/home/mike/Documents/image_gesture/leapGestRecog_small_2.h5')
-model.summary()
-
-
-# Preprocess the image into a 4D tensor
-img_path = '/home/mike/Documents/image_gesture/dset_data/test/1/frame_00_03_0001.png'
-img = image.load_img(img_path, target_size=(150,150))
-img_tensor = image.img_to_array(img)
-img_tensor = np.expand_dims(img_tensor, axis=0)
-
-# The model was trained on the inputs that were preprocessed as follows. 
-img_tensor /= 255.
-
-# The shape is (1, 150, 150, 3)
-print(img_tensor.shape)
-
-
-plt.imshow(img_tensor[0])
-plt.show()
-
-
-layer_outputs = [layer.output for layer in model.layers[:8]]
-activation_model = models.Model(inputs=model.input, outputs=layer_outputs)
-activations = activation_model.predict(img_tensor)
-
-first_layer_activation=activations[0]
-print('The 1st layer network size：',first_layer_activation.shape)
-
-# The third channel
-plt.matshow(first_layer_activation[0,:,:,3],cmap="viridis")
-plt.show()
-
-# The tenth channel
-plt.matshow(first_layer_activation[0,:,:,30],cmap="viridis")
 plt.show()
 
 
